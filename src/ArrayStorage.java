e1b85f12faae00cca14d32f7a5da083c78249862
Nikita <nikitamasslennikov@mail.ru>
2022-11-21 17:53:59 +0300

    Исправил ошибки

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,63 +1,63 @@
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
+    private int size = 0;
     Resume[] storage = new Resume[10000];
 
     void clear() {
-        for(int i = 0; i< storage.length; i++){
-            if(storage[i] != null){
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null) {
                 storage[i] = null;
-            }else{
+            } else {
                 break;
             }
         }
+        size = 0;
     }
 
     void save(Resume r) {
-        for(int i = 0; i< storage.length; i++){
-            if(storage[i] == null){
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
                 storage[i] = r;
                 break;
             }
         }
+        size++;
     }
 
     Resume get(String uuid) {
         Resume id = null;
-        for(Resume resume : storage ){
-            if(resume.uuid.equals(uuid)){
-               id = resume;
-               break;
+        for (Resume resume : storage) {
+            if (resume.uuid.equals(uuid)) {
+                id = resume;
+                break;
             }
         }
         return id;
     }
 
     void delete(String uuid) {
-        List<Resume> listStorage = new ArrayList<>();
-        for(int i = 0; i<storage.length; i++){
-            if(storage[i] != null) {
-                listStorage.add(storage[i]);
-            }else{
+        int index = 0;
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                storage[i] = null;
+                index = i;
                 break;
             }
         }
-        clear();
-        for(int i = 0; i<listStorage.size(); i++){
-            if(listStorage.get(i).uuid.equals(uuid)){
-                listStorage.remove(i);
+        for (int i = index; i < storage.length; i++) {
+            if (storage[i + 1] != null) {
+                storage[i] = storage[i + 1];
+                storage[i + 1] = null;
+            } else {
                 break;
             }
         }
 
-        for(int i = 0; i<listStorage.size(); i++){
-            storage[i] = listStorage.get(i);
-        }
+        size--;
+
     }
 
     /**
@@ -65,31 +65,25 @@ public class ArrayStorage {
      */
     Resume[] getAll() {
         int size = 0;
-        for(int i = 0; i<storage.length; i++){
-            if(storage[i] != null){
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null) {
                 size++;
-            }else{
+            } else {
                 break;
             }
         }
         Resume[] temporaryStorage = new Resume[size];
-        for(int i = 0; i<storage.length; i++){
-            if(storage[i] != null){
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null) {
                 temporaryStorage[i] = storage[i];
-            }else{
+            } else {
                 break;
             }
         }
-
         return temporaryStorage;
     }
 
     int size() {
-        int size = 0;
-        for(int i = 0; i< storage.length; i++){
-            if(storage[i] != null)
-                size++;
-        }
         return size;
     }
 }
