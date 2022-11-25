f3c08ccb81537e41f0d4ebcaa27518edd72610a6
Nikita <nikitamasslennikov@mail.ru>
2022-11-25 18:10:12 +0300

    lesson2

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -2,6 +2,7 @@ package com.urise.webapp.storage;
 
 import com.urise.webapp.model.Resume;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -10,92 +11,72 @@ import java.util.Scanner;
 public class ArrayStorage {
     private int size = 0;
     private boolean test = false;
-    Resume[] storage = new Resume[10000];
+    private final Resume[] STORAGE = new Resume[10000];
 
     public void clear() {
-        for (int i = 0; i < size; i++) {
-            storage[i] = null;
-        }
-        size = 0;
+        Arrays.fill(STORAGE, 0, size, null);
+      size = 0;
     }
 
     public void save(Resume r) {
-        String uuid = r.getUuid();
-        check(uuid);
-        if (!test) {
-            storage[size] = r;
+        int index = getIndex(r.getUuid());
+        if(index == -1){
+            STORAGE[size] = r;
             size++;
-        } else {
-            System.out.println("ERROR: " + "Resume " + r + " already exist");
+        }else{
+            System.out.println("ERROR:" + " resume " + r.getUuid() + " already exist!");
         }
-        test = false;
     }
 
     public void update(Resume r) {
-        String uuid = r.getUuid();
-        check(uuid);
-        if (test) {
+        int index = getIndex(r.getUuid());
+        if(index == -1){
+            System.out.println("ERROR:" + " resume " + r.getUuid() + " doesn't exist!");
+        }else{
             System.out.print("Enter updated resume: ");
             Scanner scanner = new Scanner(System.in);
             String newUuid = scanner.nextLine();
             r.setUuid(newUuid);
-        } else {
-            System.out.println("ERROR: " + "Resume " + uuid + " doesn't exist");
         }
-        test = false;
     }
 
     public Resume get(String uuid) {
-        check(uuid);
-        if (test) {
-            for (int i = 0; i < size; i++) {
-                if (storage[i].getUuid().equals(uuid)) {
-                    return storage[i];
-                }
-            }
-        } else {
-            System.out.println("ERROR: " + "Resume " + uuid + " doesn't exist");
+        int index = getIndex(uuid);
+        if(index == -1){
+            System.out.println("ERROR:" + " resume " + uuid + " doesn't exist!");
+            return null;
         }
-        test = false;
-        return null;
+        return STORAGE[index];
     }
 
     public void delete(String uuid) {
-        check(uuid);
-        if (test) {
-            for (int i = 0; i < size; i++) {
-                if (storage[i].getUuid().equals(uuid)) {
-                    storage[i] = storage[size - 1];
-                    storage[size - 1] = null;
-                    size--;
-                }
-            }
-        } else {
-            System.out.println("Resume " + uuid + " doesn't exist");
+        int index = getIndex(uuid);
+        if(index == -1){
+            System.out.println("ERROR:" + " resume " + uuid + " doesn't exist!");
+        }else{
+            STORAGE[index] = STORAGE[size - 1];
+            STORAGE[size - 1] = null;
+            size--;
         }
-        test = false;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     public Resume[] getAll() {
-        Resume[] temporaryStorage = new Resume[size];
-        for (int i = 0; i < size; i++) {
-            temporaryStorage[i] = storage[i];
-        }
-        return temporaryStorage;
+        return Arrays.copyOfRange(STORAGE, 0, size);
     }
 
     public int size() {
         return size;
     }
 
-    private void check(String uuid) {
-        for (int i = 0; i < size; i++) {
-            if (storage[i].getUuid().equals(uuid)) {
-                test = true;
+    private int getIndex(String uuid){
+        for(int i = 0; i<size; i++){
+            if(STORAGE[i].getUuid().equals(uuid)){
+                return i;
             }
         }
+        return -1;
     }
 }
